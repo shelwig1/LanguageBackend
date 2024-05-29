e38d5b28e4b6432dd67680484475965636d5ed65
Sean <sean.helwig@gmail.com>
2024-05-29 16:16:36 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const translate = require('./translate')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/', async (req, res) => {
+    const data = req.body
+    console.log("Server - data received:", data)
+    const responseData = []
+    console.log("Recieved a post request ", data)
+    console.log(data.length)
+    for (const word in data) {
+        //console.log("Word: ", data[word])
+        //const processedWord = data[word]
+        // Uncomment this and everything should work correctly
+        const processedWord = await translate.translateText(data[word])
+        responseData.push(processedWord)
+    }
+    //res.body = JSON.stringify(responseData)
+    res.json(responseData)
+
+    res.status(200)
+    //console.log("Server - response data: ", res.body)
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ translate.js
@@ -0,0 +1,38 @@
+const {TranslationServiceClient} = require('@google-cloud/translate');
+
+const credentialsBase64 = process.env.GOOGLE_AUTH;
+const credentialsJSON = JSON.parse(Buffer.from(credentialsBase64, 'base64').toString('ascii'));
+
+// Instantiates a client
+const translationClient = new TranslationServiceClient({
+    credentials : credentialsJSON
+});
+
+const projectId = 'virtual-cubist-399422';
+const location = 'global';
+/* const text = 'Hello, world!'; */
+
+async function translateText(text) {
+    // Construct request
+
+
+    const request = {
+        parent: `projects/${projectId}/locations/${location}`,
+        contents: [text],
+        mimeType: 'text/plain', // mime types: text/plain, text/html
+        sourceLanguageCode: 'en',
+        targetLanguageCode: 'ar',
+    };
+
+    // Run request
+    const [response] = await translationClient.translateText(request);
+
+
+    for (const translation of response.translations) {
+        console.log(`Translation: ${translation.translatedText}`);
+        return(translation.translatedText)
+    }
+}
+
+
+module.exports = {translateText}
