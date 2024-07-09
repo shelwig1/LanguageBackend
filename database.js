f95418994fbc3ad080640e29d0ae0bf94e1b0da7
Sean <sean.helwig@gmail.com>
2024-07-09 15:10:06 -0400

    added database route

---
+++ database.js
@@ -0,0 +1,39 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+
+async function createTable() {
+    // SQL query to create a table
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        email VARCHAR(100) UNIQUE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+  
+    try {
+      // Connect to the database and execute the query
+      const client = await pool.connect();
+      try {
+        await client.query(createTableQuery);
+        console.log('Table created successfully!');
+      } finally {
+        // Release the client back to the pool
+        client.release();
+      }
+    } catch (err) {
+      console.error('Error creating table:', err.stack);
+    }
+  }
+  
+  // Run the function to create the table
+  //createTable().catch(err => console.error('Error in createTable:', err));
+
+
+export { createTable }
--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const executeDatabase = require('./database.js')
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/database', (req, res) => {
+  //execute database.js
+  //executeDatabase()
+  res.send("Database code executed")
+})
 
 app.post('/', async (req, res) => {
     const data = req.body
@@ -20,6 +28,19 @@ app.post('/', async (req, res) => {
         //console.log("Word: ", data[word])
         //const processedWord = data[word]
         // Uncomment this and everything should work correctly
+
+        /*
+          Who is sending the request?
+
+          Is this word in the database?
+            Yes - return the word with the correct language
+            No - run translateText and log the information in the database
+
+            Increment the count of the word occuring in the database
+
+            Return everything back over
+        */
+
         const processedWord = await translate.translateText(data[word], targetLanguageCode)
         responseData.push(processedWord)
     }
